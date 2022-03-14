2380ac2a11ff7b965cb2caa67892175512c5b4a9
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-14 19:51:29 +0530

    refactor: remove old cloud commands infrastructure

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = require("./router");
-
-const app = express();
-
-app.use(router);
-
-module.exports = app;
--- src/cloud/utils.js
+++
@@ -1,9 +0,0 @@
-const unixTimestampReponse = require("../shared/utils/unix-timestamp");
-
-const unixTimestamp = (_, res) => {
-  res.json(unixTimestampReponse);
-};
-
-module.exports = {
-  unixTimestamp,
-};
