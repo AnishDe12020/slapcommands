c16f7d169ab0e9221a00477d56614d98a73585c2
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-15 15:23:21 +0530

    feat(docsearch-chakra-ui): added a move action for local commands

--- src/local/docsearch/chakra-ui-docs.js
+++ src/local/docsearch/chakra-ui-docs.js
@@ -1,2 +1,17 @@
-const response = require("../../shared/docsearch/chakra-ui-docs");
+const {
+  rootResponse,
+  itemResponse,
+} = require("../../shared/docsearch/chakra-ui-docs");
+const { parseArgs } = require("../../utils/parseArgs");
+
+const args = parseArgs(process.argv);
+
+let response;
+
+if (args.item) {
+  response = itemResponse(args.item);
+} else {
+  response = rootResponse();
+}
+
 console.log(JSON.stringify(response));
