0b29bff1f4e3e3034091776886b7e8804d074527
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-03-21 11:56:06 +0530

    feat(docsearch,slapdash-platform): add move action support

--- src/local/docsearch/slapdash-platform-docs.js
+++ src/local/docsearch/slapdash-platform-docs.js
@@ -1,3 +1,18 @@
-const response = require("../../shared/docsearch/slapdash-platform-docs");
+const {
+  rootResponse,
+  itemResponse,
+} = require("../../shared/docsearch/slapdash-platform-docs");
+
+const { parseArgs } = require("../../utils/parseArgs");
+
+const args = parseArgs(process.argv);
+
+let response;
+
+if (args.item) {
+  response = itemResponse(args.item);
+} else {
+  response = rootResponse();
+}
 
 console.log(JSON.stringify(response));
--- src/shared/docsearch/slapdash-platform-docs.js
+++ src/shared/docsearch/slapdash-platform-docs.js
@@ -1,16 +1,66 @@
 const slapdashPlatfomrDocsItmes = require("../../data/docsearch/slapdash-platform-docs.json");
 
-module.exports = {
-  view: {
-    type: "list",
-    options: slapdashPlatfomrDocsItmes.map((item) => ({
-      title: item.title,
-      subtitle: item.subtitle,
-      action: {
-        type: "open-url",
-        url: item.url,
-      },
-      icon: "https://slapdash.com/favicon-180x180.2sB.png",
-    })),
-  },
+const slapdashLogo = "https://slapdash.com/favicon-180x180.2sB.png";
+
+const rootResponse = () => {
+  return {
+    view: {
+      type: "list",
+      options: slapdashPlatfomrDocsItmes.map((item) => ({
+        title: item.title,
+        subtitle: item.subtitle,
+        action: {
+          type: "open-url",
+          url: item.url,
+        },
+        icon: slapdashLogo,
+        moveAction: {
+          type: "add-param",
+          name: "item",
+          value: `${item.title}&${item.url}`,
+        },
+      })),
+    },
+  };
 };
+
+const itemResponse = (itemString) => {
+  const [itemName, itemUrl] = itemString.split("&");
+  return {
+    tokens: [
+      {
+        paramName: "item",
+        label: itemName,
+        icon: slapdashLogo,
+      },
+    ],
+    view: {
+      type: "list",
+      options: [
+        {
+          title: "Open in browser",
+          action: {
+            type: "open-url",
+            url: itemUrl,
+          },
+        },
+        {
+          title: "Copy URL to clipboard",
+          action: {
+            type: "copy",
+            value: itemUrl,
+          },
+        },
+        {
+          title: "Paste URL",
+          action: {
+            type: "paste",
+            value: itemUrl,
+          },
+        },
+      ],
+    },
+  };
+};
+
+module.exports = { rootResponse, itemResponse };
